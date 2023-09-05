9a07e47b79bfd6449c7396e4378b773c31374d50
Ratanjot <sahil@trigvent.com>
2023-09-05 17:27:40 +0530

    cascade dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import GlobalStyle from "./styles/global"
-import Navbar from "./components/Navbar"
-import Menubar from "./components/Menubar"
-import PageBody from "./components/PageBody"
+// import Navbar from "./components/Navbar"
+// import Menubar from "./components/Menubar"
+// import PageBody from "./components/PageBody"
+import CascadeDropdownDemo from './components/CascadeDropdown/countryCascadeDropdown';
 
 const App:React.FC = ()=> (
   <> 
     <GlobalStyle/>
-    <Navbar/>
-    <Menubar/>
-    <PageBody/>
+    <CascadeDropdownDemo/>
   </>
 )
 
